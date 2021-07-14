a8350c251fc3e144e809ed1dfacea16588e80d28
WilliamsJ3 <WilliamsJ3@USA-CND802283R>
2021-07-14 12:46:32 -0500

    jason commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StevenChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> treeValues = new Dictionary<int, int>();
+            treeValues.Add(1, 2);
+            treeValues.Add(3, 2);
+            treeValues.Add(2, 12);
+            treeValues.Add(5, 2);
+            System.Console.WriteLine(treeValidator(treeValues));
+        }
+        public static bool treeValidator(Dictionary<int, int> tree)
+        {
+            int parentcount = 0;
+            Dictionary<int, int> tracker = new Dictionary<int, int>();
+            foreach (KeyValuePair<int, int> node in tree)
+            {               
+                if (tracker.ContainsValue(node.Value))
+                {
+                    parentcount++;
+                    if (parentcount > 1)
+                    {
+                        return false;
+                    }
+                }
+                tracker.Add(node.Key, node.Value);
+            }
+            return true;
+        }
+    }
+}
+
